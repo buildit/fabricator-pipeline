87d0ecabbe6a7a4c14e33c5b0ff3af8236637ae8
kashifmasood <kashifisin@icloud.com>
2018-04-23 21:30:24 -0600

    Added output for webserver IP public address

--- modules/services/webserver/outputs.tf
+++ modules/services/webserver/outputs.tf
@@ -29,3 +29,7 @@ output "vpc_security_group_id" {
 output "vpc_private_subnet_id" {
   value = "${aws_subnet.subnet_private.id}"
 }
+
+output "webserver_public_ip" {
+  value = "${aws_instance.web-server.public_ip}"
+}
